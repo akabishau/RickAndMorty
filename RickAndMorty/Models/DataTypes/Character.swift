78c0bb0c9f49baa8708234bf136c4eb176d92ab5
akabishau <aleksey.kabishau@icloud.com>
2022-12-26 22:35:50 -0500

    feat: add models to represent the app's data types

---
+++ RickAndMorty/Models/DataTypes/Character.swift
@@ -0,0 +1,50 @@
+//
+//  Character.swift
+//  RickAndMorty
+//
+//  Created by Aleksey Kabishau on 12/26/22.
+//
+
+import Foundation
+
+
+struct RMCharacter: Codable {
+	let id: Int
+	let name: String
+	let status: String
+	let species: String
+	let type: String
+	let gender: CharacterGender
+	let origin: Origin
+	let location: SingleLocation
+	let image: String
+	let episode: [String]
+	let url: String
+	let created: String
+}
+
+
+struct Origin: Codable {
+	let name: String
+	let url: String
+	
+}
+
+struct SingleLocation: Codable {
+	let name: String
+	let url: String
+}
+
+enum CharacterGender: String, Codable {
+	case male = "Male"
+	case female = "Female"
+	case genderless = "Genderless"
+	case `unknown` = "unknown" //using ` to avoid potential issues with Swift keyword "unknown"
+}
+
+
+enum CharacterStatus: String, Codable {
+	case alive = "Alive"
+	case dead = "Dead"
+	case `unknown` = "unknown" // using ` to avoid potential issues with Swift keyword "unknown"
+}
---
+++ RickAndMorty/Models/DataTypes/Episode.swift
@@ -0,0 +1,18 @@
+//
+//  Episode.swift
+//  RickAndMorty
+//
+//  Created by Aleksey Kabishau on 12/26/22.
+//
+
+import Foundation
+
+struct RMEpisode: Codable, RMEpisodeDataRender {
+	let id: Int
+	let name: String
+	let air_date: String
+	let episode: String
+	let characters: [String]
+	let url: String
+	let created: String
+}
